eb6abdf5f3127d548721ff8975246712bd56c1e3
steve jordan <steve@stevejordandigital.co.uk>
2015-06-10 21:21:37 +0100

    init logfile

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ PLUGIN_DIR=$EOC_DOTDIR/plugins
 
 REPO_DIR=$PWD
 cd $PLUGIN_DIR && find $REPO_DIR -name \*.py -exec ln -s {} \; && cd $REPO_DIR
+
+# init logfile
+LOGFILE="$PLUGIN_DIR/eoc_plugin.log"
+if [ ! -x $LOGFILE ]; then
+    touch $LOGFILE
+fi
