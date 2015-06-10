4e644c05f9f75d80071ba36315f14ec146548f87
steve jordan <steve@stevejordandigital.co.uk>
2015-06-10 21:27:56 +0100

    better symlink exists test

--- install.sh
+++ install.sh
@@ -6,10 +6,17 @@ EOC_DOTDIR=~/.enemies-of-carlotta
 PLUGIN_DIR=$EOC_DOTDIR/plugins
 
 REPO_DIR=$PWD
-cd $PLUGIN_DIR && find $REPO_DIR -name \*.py -exec ln -s {} \; && cd $REPO_DIR
+cd $PLUGIN_DIR 
+SRC_FILES=$(find $REPO_DIR -name \*.py)
+for file in $SRC_FILES; do
+    if [ ! -h $file ]; then
+        ln -s $file
+    fi
+done
+cd $REPO_DIR
 
 # init logfile
 LOGFILE="$PLUGIN_DIR/eoc_plugin.log"
-if [ ! -x $LOGFILE ]; then
+if [ ! -e $LOGFILE ]; then
     touch $LOGFILE
 fi
