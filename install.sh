56953e5b0eb2a1298f9ca163d44bcdb889aa191c
steve jordan <steve@stevejordandigital.co.uk>
2015-06-10 21:18:29 +0100

    corrects plugin path

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # install file for plugin
 
 EOC_DOTDIR=~/.enemies-of-carlotta
-PLUGIN_DIR=$EOC_DIR/plugins
+PLUGIN_DIR=$EOC_DOTDIR/plugins
 
 REPO_DIR=$PWD
 cd $PLUGIN_DIR && find $REPO_DIR -name \*.py -exec ln -s {} \; && cd $REPO_DIR
